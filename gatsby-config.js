46ee4622fd3fb2c8dbd288c7559acef8f0f86d35
Kevin O'Connor <kevpedia@gmail.com>
2021-01-01 12:34:02 -0500

    added spaceId to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: "AudioC0RE",
+    title: 'AudioC0RE',
   },
   plugins: [
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
-        accessToken: "tQ9ua8F7bSvtT-rSe3ousPBFFC-MsYmDHlMWTdt-mdE",
-        spaceId: "",
+        accessToken: 'tQ9ua8F7bSvtT-rSe3ousPBFFC-MsYmDHlMWTdt-mdE',
+        spaceId: '6w7ipsusex2z',
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
