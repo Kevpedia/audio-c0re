7122a4305dab69cf6e53b263fabc3150640741b7
Kevin O'Connor <kevpedia@gmail.com>
2021-01-01 12:45:38 -0500

    changed env variables to match names in gatsby cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        accessToken: process.env.CONTENTFUL_TOKEN,
-        spaceId: process.env.CONTENTFUL_SPACEID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
       },
     },
     'gatsby-plugin-sharp',
