52f81bebaf6451c632143827fbfa823f0a8df585
Kevin O'Connor <kevpedia@gmail.com>
2021-02-20 11:09:22 -0500

    fixed icon path

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#16BFBD`,
         display: `standalone`,
-        icon: `/src/images/icon.png`,
+        icon: `src/images/icon.png`,
       },
     },
     'gatsby-plugin-offline',
